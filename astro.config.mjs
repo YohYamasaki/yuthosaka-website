0f367b9b00981050e1e7ea7ca9340c41c8d3213f
YohYamasaki <y.yamasaki@tutanota.com>
2024-10-11 23:48:24 +0200

    setup linter & precommit formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), icon(), solidJs()],
+  site: "https://yutohosaka.com",
+  integrations: [mdx(), sitemap(), icon(), solidJs(), compressor()],
+  trailingSlash: "never",
   build: {
     assets: "assets",
+    format: "directory",
   },
 });
