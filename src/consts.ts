fe7d0356b77b3dc484af36ba02a7da5ed1251e39
YohYamasaki <y.yamasaki@tutanota.com>
2024-10-11 23:48:52 +0200

    minor refactoring & styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Yuto Hosaka";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Yuto Hosaka (MPIDS-LMP)";
+export const SITE_DESCRIPTION =
+  "Department of Living Matter Physics, Max Planck Institute for Dynamics and Self-Organization (MPIDS-LMP). My interests are soft active matter and low Reynolds number hydrodynamics, especially chiral active fluids.";
